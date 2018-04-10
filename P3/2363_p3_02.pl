d1d335ee7ab2bf6832490d3ff320926a4e2e12b2
Anton Oellerer <aoellerer@gmail.com>
2018-04-10 21:28:21 +0200

    implementing 3

--- P3/2363_p3_02.pl
+++ P3/2363_p3_02.pl
@@ -1,3 +1,4 @@
+%Ejercicio 1
 pertenece(X, [X|_]).
 pertenece(X, [_|Rs]) :- pertenece(X, Rs).
 
@@ -10,3 +11,30 @@ pertenece_m(X, [Y|_]) :-
 
 pertenece_m(X, [_|Rs]) :-
 	pertenece_m(X, Rs).
+
+
+%Ejercicio 2
+
+concatena([], L, L).
+concatena([X|L1], L2, [X|L3]) :-
+	concatena(L1, L2, L3).
+
+invierte([], []).
+
+invierte([H|T], R) :-
+	invierte(T, Ls),
+	concatena(Ls, [H], R).
+
+%Ejercicio 3
+
+%TODO: ask
+
+insert(X-P, [], [X-P]).
+
+insert(X-P, [Y-P1|Ls], [X-P,Y-P1|Ls]) :-
+	P =< P1.
+
+insert(X-P, [Y-P1|Ls], R) :-
+	P > P1,
+	insert(X-P, Ls, T),
+	concatena([Y-P1], T, R).
